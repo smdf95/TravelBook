d487adeaa4aed5efcc6523e598732f97d0babf19
smdf95 <s.fisher95@hotmail.com>
2024-03-15 15:37:02 +0000

    fixing settings for render

--- TravelBook/settings.py
+++ TravelBook/settings.py
@@ -95,16 +95,8 @@ WSGI_APPLICATION = 'TravelBook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
-database_url = os.environ.get('TRAVELBOOK_DB_URL')
-if database_url:
-    DATABASES["default"] = dj_database_url.parse(database_url)
+DATABASES = {"default": dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")}
 
 
 # Password validation
